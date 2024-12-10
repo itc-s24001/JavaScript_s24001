f0b57955ab7d8b2198677b7b9e1e8ddc6ea341e9
itc-s24001 <s24001@std.it-college.ac.jp>
2024-12-10 15:46:33 +0900

    CH05-2 liタグ作成

---
+++ CH05/CH05-02/script.js
@@ -0,0 +1,11 @@
+var fruits = ['リンゴ', 'モモ', 'バナナ'];
+var ul = document.getElementById('fruit_list');
+var li = document.createElement('li');
+
+for(var cnt = 0; cnt < fruits.length; cnt++) {
+    var li = document.createElement('li');
+    var text = document.createTextNode(fruits[cnt]);
+    li.appendChild(text);
+    ul.appendChild(li);
+  }
+  
